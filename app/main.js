710406f37a169e429efee75f901aaa0f23a32caf
Sandor Trombitas <tis.sandor@gmail.com>
2023-10-22 20:04:32 +0300

    pass test #2

--- app/main.js
+++ app/main.js
@@ -8,6 +8,12 @@ const server = net.createServer(socket => {
     socket.end();
     server.close();
   });
+  socket.on('data', data => {
+    socket.write('HTTP/1.1 200 OK\r\n\r\n');
+    socket.end();
+  });
+    
+
 });
 
 server.listen(4221, 'localhost');
