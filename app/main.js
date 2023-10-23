41bfb739e87972430e4fbfa660a727329ad7427a
Sandor Trombitas <tis.sandor@gmail.com>
2023-10-23 12:15:04 +0300

    Pass 4

--- app/main.js
+++ app/main.js
@@ -3,6 +3,14 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+const buildTextResponse = (statusCode, body) => {
+  let response = 'HTTP/1.1 200 OK\r\n';
+  response = response.concat('Content-Type: text/plain\r\n');
+  response = response.concat(`Content-Length: ${body.length}\r\n`);
+  response = response.concat('\r\n');
+  return response.concat(body.toString());
+};
+  
 const getData = socket => data => {
   if (!data instanceof Buffer) {
     //throw new Error('Data must be a Buffer');
@@ -15,6 +23,11 @@ const getData = socket => data => {
 
   if (path === '/') {
     socket.write('HTTP/1.1 200 OK\r\n\r\n');
+    return socket.end();
+  }
+  if (path.startsWith('/echo/')) {
+    const body = path.split('/echo/')[1];
+    socket.write (buildTextResponse(200, body));
     return socket.end();
   }
 
@@ -30,3 +43,4 @@ const server = net.createServer(socket => {
 });
 
 server.listen(4221, 'localhost');
+
