38902f3a4d0dbfe21beb9f8270cf97704737ce10
Sandor Trombitas <tis.sandor@gmail.com>
2023-10-23 14:58:53 +0300

    pass 5

--- app/main.js
+++ app/main.js
@@ -3,6 +3,42 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+const parseRequest = data => {
+  const request = data.toString().split(/[\r\n]+/).reduce((acc, line, index) => {
+    if(index === 0) {
+      const [method, path] = line.split(' ');
+
+      return {
+        ...acc,
+        method,
+        path
+      }
+    }
+
+    if (line.startsWith('Host:')) {
+      const [_, host] = line.split(' ');
+
+      return {
+        ...acc,
+        host
+      }
+    }
+
+    if (line.startsWith('User-Agent')) {
+      const [_, userAgent] = line.split(' ');
+
+      return {
+        ...acc,
+        userAgent
+      }
+    }
+
+    return acc;
+  }, {});
+
+  return request;
+}
+
 const buildTextResponse = (statusCode, body) => {
   let response = 'HTTP/1.1 200 OK\r\n';
   response = response.concat('Content-Type: text/plain\r\n');
@@ -10,24 +46,27 @@ const buildTextResponse = (statusCode, body) => {
   response = response.concat('\r\n');
   return response.concat(body.toString());
 };
-  
-const getData = socket => data => {
+
+const handleData = socket => data => {
   if (!data instanceof Buffer) {
     //throw new Error('Data must be a Buffer');
     console.log('Data must be a Buffer');
     socket.write('HTTP/1.1 400 Bad Request\r\n\r\n');
     return socket.end();
   }
-  const request = data.toString().split(/[\r\n]+/);
-  const [_, path] = request[0].split(' ');
+  const request = parseRequest(data);
 
-  if (path === '/') {
+  if (request.path === '/') {
     socket.write('HTTP/1.1 200 OK\r\n\r\n');
     return socket.end();
   }
-  if (path.startsWith('/echo/')) {
-    const body = path.split('/echo/')[1];
+  if (request.path.startsWith('/echo/')) {
+    const body = request.path.split('/echo/')[1];
     socket.write (buildTextResponse(200, body));
+    return socket.end();
+  }
+  if (request.path.startsWith('/user-agent')) {
+    socket.write(buildTextResponse(200, request.userAgent));
     return socket.end();
   }
 
@@ -39,7 +78,7 @@ const server = net.createServer(socket => {
     socket.end();
     server.close();
   });
-  socket.on('data', getData(socket));
+  socket.on('data', handleData(socket));
 });
 
 server.listen(4221, 'localhost');
